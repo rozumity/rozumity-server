6ca703c59c3b2219fd643f0885a110504cd5f481
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-03-03 00:47:16 +0000

    add user deletion on profiles deletion

--- rozumity/accounts/signals.py
+++ rozumity/accounts/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.utils.translation import gettext_lazy as _
 from django.db.utils import IntegrityError
 
@@ -15,6 +15,13 @@ async def create_user_profile(sender, instance, created, **kwargs):
         await profile_model.objects.acreate(user=instance)
     except IntegrityError:
         pass
+
+
+@receiver(post_delete, sender=ClientProfile, dispatch_uid='clientProfile.delete_user')
+@receiver(post_delete, sender=ExpertProfile, dispatch_uid='expertProfile.delete_user')
+@receiver(post_delete, sender=StaffProfile, dispatch_uid='staffProfile.delete_user')
+async def delete_expert_profile(sender, instance, created, **kwargs):
+    await sender.adelete(id=instance.user.id)
 
 
 #@receiver(post_save, sender=User, dispatch_uid='user.save_user_profile')
