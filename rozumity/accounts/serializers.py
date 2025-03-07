3209682af00485846c708db64f5f53908612007f
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-03-07 01:03:52 +0000

    add profile serializers

--- rozumity/accounts/serializers.py
+++ rozumity/accounts/serializers.py
@@ -1,5 +1,12 @@
 from adrf.serializers import ModelSerializer
-from .models import User, ClientProfile, ExpertProfile, University, Speciality
+from .models import User, ClientProfile, ExpertProfile, University, Speciality, Education
+
+# --- Education
+
+class SpecialitySerializer(ModelSerializer):
+    class Meta:
+        model = Speciality
+        fields = ('title', 'code')
 
 
 class UniversitySerializer(ModelSerializer):
@@ -8,7 +15,38 @@ class UniversitySerializer(ModelSerializer):
         fields = ('name', 'country')
 
 
-class SpecialitySerializer(ModelSerializer):
+class EducationSerializer(ModelSerializer):
+    university = UniversitySerializer()
+    speciality = SpecialitySerializer()
     class Meta:
-        model = Speciality
-        fields = ('title', 'code')
+        model = Education
+        fields = ('university', 'degree', 'speciality', 'date_start', 'date_end')
+
+# Education ---
+# --- Profile
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("email", "is_staff", "is_client", "is_expert", "is_active", "date_joined")
+        read_only_fields = fields.copy()
+
+
+class ClientProfileSerializer(ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = ClientProfile
+        fields = ('first_name', 'last_name', 'gender', 'country', 'date_birth', 'user')
+        read_only_fields = ('date_birth', 'user')
+
+
+class ExpertProfileSerializer(ModelSerializer):
+    user = UserSerializer()
+    education = EducationSerializer(many=True)
+    
+    class Meta:
+        model = ExpertProfile
+        fields = ('first_name', 'last_name', 'gender', 'country', 'date_birth', 'user', 'education', 'education_extra', 'countries_allowed')
+        read_only_fields = ('date_birth', 'user')
+
+# --- Profile
