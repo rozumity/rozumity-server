c1b89d1f4a07e3330999325273ac00a92ba06b84
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-04-22 19:56:35 +0000

    Create model serializers in serializers.py

--- rozumity/accounts/serializers.py
+++ rozumity/accounts/serializers.py
@@ -1,26 +1,35 @@
-from adrf.serializers import ModelSerializer
-from .models import User, ClientProfile, ExpertProfile, University, Speciality, Education
+from adrf.serializers import ModelSerializer, Serializer
+from asgiref.sync import sync_to_async, async_to_sync
+
+from rest_framework.serializers import CharField, HyperlinkedIdentityField
+
+from .models import *
+
 
 # --- Education
 
 class SpecialitySerializer(ModelSerializer):
     class Meta:
         model = Speciality
-        fields = ('title', 'code')
+        fields = '__all__'
 
 
 class UniversitySerializer(ModelSerializer):
+    country = CharField(max_length=2, allow_blank=False)
+
     class Meta:
         model = University
-        fields = ('name', 'country')
+        fields = '__all__'
 
 
 class EducationSerializer(ModelSerializer):
     university = UniversitySerializer()
     speciality = SpecialitySerializer()
+
     class Meta:
         model = Education
-        fields = ('university', 'degree', 'speciality', 'date_start', 'date_end')
+        fields = "__all__"
+        read_only_fields = ('university', "speciality")
 
 # Education ---
 # --- Profile
@@ -28,25 +37,67 @@ class EducationSerializer(ModelSerializer):
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ("email", "is_staff", "is_client", "is_expert", "is_active", "date_joined")
+        fields = ["email", "is_staff", "is_client", "is_expert", "is_active", "date_joined"]
         read_only_fields = fields.copy()
 
 
 class ClientProfileSerializer(ModelSerializer):
-    user = UserSerializer()
+    user = HyperlinkedIdentityField(view_name="accounts:user")
+    country = CharField(max_length=2, allow_blank=False)
+
     class Meta:
         model = ClientProfile
-        fields = ('first_name', 'last_name', 'gender', 'country', 'date_birth', 'user')
+        fields = "__all__"
         read_only_fields = ('date_birth', 'user')
 
 
 class ExpertProfileSerializer(ModelSerializer):
-    user = UserSerializer()
-    education = EducationSerializer(many=True)
-    
+    user = HyperlinkedIdentityField(view_name="accounts:user")
+    education = HyperlinkedIdentityField(view_name="accounts:education")
+
     class Meta:
         model = ExpertProfile
-        fields = ('first_name', 'last_name', 'gender', 'country', 'date_birth', 'user', 'education', 'education_extra', 'countries_allowed')
+        fields = "__all__"
+        read_only_fields = ('date_birth', 'user')
+
+
+class StaffProfileSerializer(ModelSerializer):
+    user = HyperlinkedIdentityField(view_name="accounts:user")
+
+    class Meta:
+        model = ExpertProfile
+        fields = "__all__"
         read_only_fields = ('date_birth', 'user')
 
 # --- Profile
+# Subscription ---
+
+class SubscriptionPlanSerializer(ModelSerializer):
+    class Meta:
+        model = SubscriptionPlan
+        fields = "__all__"
+        read_only_fields = ('owner_type',)
+
+
+class TherapyContractSerializer(ModelSerializer):
+    user = HyperlinkedIdentityField(view_name="accounts:user")
+    client = HyperlinkedIdentityField(view_name="accounts:client-profile")
+    expert = HyperlinkedIdentityField(view_name="accounts:expert-profile")
+
+    class Meta:
+        model = ExpertProfile
+        fields = "__all__"
+        read_only_fields = ('client', 'expert', "date_start")
+
+# --- Subscription
+# --- Diary
+
+class DiarySerializer(ModelSerializer):
+    client = HyperlinkedIdentityField(view_name="accounts:client-profile")
+
+    class Meta:
+        model = Diary
+        fields = "__all__"
+        read_only_fields = ('client', 'date_start')
+
+# Diary ---
