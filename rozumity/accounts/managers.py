7477c762f4a03dddc036e09c7b293d9b2aca8b5b
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-04-22 21:00:49 +0000

    Replace and rename the user manager

--- rozumity/accounts/managers.py
+++ rozumity/accounts/managers.py
@@ -2,17 +2,10 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
+class EmailUserManager(BaseUserManager):
     use_in_migrations = True
-    
+
     def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given email and password.
-        """
         if not email:
             raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
@@ -22,9 +15,6 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
