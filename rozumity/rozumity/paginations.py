f71082d3f8513d7d9170f3a2c9a3287b51b07d8f
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-04-22 19:57:46 +0000

    Add new paginations in paginations.py

--- rozumity/rozumity/paginations.py
+++ rozumity/rozumity/paginations.py
@@ -1,191 +1,130 @@
-from contextlib import suppress
-from django.utils.translation import gettext_lazy as _
-from django.contrib.sites.shortcuts import get_current_site
-from rest_framework.settings import api_settings
-from rest_framework.response import Response
-from rest_framework.utils.urls import remove_query_param, replace_query_param
-from asgiref.sync import sync_to_async
+"""
+Pagination serializers determine the structure of the output that should
+be used for paginated responses.
+"""
+from django.core.paginator import InvalidPage
+from rest_framework.exceptions import NotFound
+from rest_framework.pagination import (
+    _reverse_ordering, PageNumberPagination as DRFPageNumberPagination,
+    LimitOffsetPagination as DRFLimitOffsetPagination,
+    CursorPagination as DRFCursorPagination
+)
 
 
-remove_query_param = sync_to_async(remove_query_param)
-replace_query_param = sync_to_async(replace_query_param)
-get_current_site = sync_to_async(get_current_site)
-
-
-class LimitOffsetAsyncPagination:
-    default_limit = api_settings.PAGE_SIZE
-    limit_query_param = 'page[limit]'
-    limit_query_description = _('Number of results to return per page.')
-    offset_query_param = 'page[offset]'
-    offset_query_description = _('The initial index from which to return the results.')
-    max_limit = None
-    current_site = None
-
-    @staticmethod
-    async def encode_url_parameters(url):
-        return url.replace('%5B', '[').replace('%5D', ']')
-    
-    @staticmethod
-    async def positive_int(integer_string, strict=False, cutoff=None):
-        ret = int(integer_string)
-        if ret < 0 or (ret == 0 and strict):
-            raise ValueError()
-        if cutoff:
-            return min(ret, cutoff)
-        return ret
-    
-    async def get_absolute_uri(self):
-        return await sync_to_async(self.request.build_absolute_uri)()
-    
-    async def paginate_queryset(self, queryset, request):
+class PageNumberPagination(DRFPageNumberPagination):
+    async def paginate_queryset(self, queryset, request, view=None):
         self.request = request
-        self.limit = await self.get_limit(request)
-        if self.limit is None:
+        page_size = self.get_page_size(request)
+        if not page_size:
             return None
-        self.count = await self.get_count(queryset)
-        self.offset = await self.get_offset(request)
-        if self.count == 0 or self.offset > self.count:
-            return await sync_to_async(queryset.model.objects.none)()
-        queryset = queryset[self.offset:self.offset + self.limit]
-        return queryset
 
-    async def get_paginated_response(self, data):
-        links = {
-            'self': await self.encode_url_parameters(
-                await sync_to_async(self.request.build_absolute_uri)()
-            )
-        }
-        next = await self.get_next_link()
-        prev = await self.get_previous_link()
-        last = await self.get_last_link()
-        if next:
-            links['next'] = next
-        if prev:
-            links['prev'] = prev
-        if last != links['self']:
-            links['last'] = last
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = self.get_page_number(request, paginator)
+
         try:
-            return Response({'links': links, **data})
-        except TypeError:
-            raise TypeError('Serializer data must be a valid dictionary.')
-
-    async def get_paginated_response_schema(self, schema=None):
-        schema = schema if schema else {
-            'data': {
-                'type': 'list',
-                'nullable': False,
-                'format': 'list_objects_jsonapi',
-                'example': [{
-                    'type': 'account', 'id': 1, 'attributes': {}, 
-                    'relationships': {'profile': {'type': 'profile', 'id': 1}}
-                }]
-            },
-            'included': {
-                'type': 'list',
-                'nullable': False,
-                'format': 'list_objects_jsonapi',
-                'example': [{'type': 'profile', 'id': 1, 'attributes': {}}]
-            }
-        }
-        return {
-            'links': {
-                'self': {
-                    'type': 'string',
-                    'nullable': False,
-                    'format': 'uri',
-                    'example': f'http://api.example.org/accounts/?{self.offset_query_param}=200&{self.limit_query_param}=100',
-                },
-                'prev': {
-                    'type': 'string',
-                    'nullable': True,
-                    'format': 'uri',
-                    'example': f'http://api.example.org/accounts/?{self.offset_query_param}=100&{self.limit_query_param}=100',
-                },
-                'next': {
-                    'type': 'string',
-                    'nullable': True,
-                    'format': 'uri',
-                    'example': f'http://api.example.org/accounts/?{self.offset_query_param}=300&{self.limit_query_param}=100',
-                },
-                'last': {
-                    'type': 'string',
-                    'nullable': False,
-                    'format': 'uri',
-                    'example': f'http://api.example.org/accounts/?{self.offset_query_param}=400&{self.limit_query_param}=100',
-                }
-            }, **schema
-        }
-    
-    async def get_next_link(self):
-        if self.offset + self.limit >= self.count:
-            return None
-        else:
-            url = await replace_query_param(
-                await self.get_absolute_uri(), 
-                self.offset_query_param, 
-                self.offset + self.limit
+            self.page = paginator.page(page_number)
+        except InvalidPage as exc:
+            msg = self.invalid_page_message.format(
+                page_number=page_number, message=str(exc)
             )
-        if self.limit == self.default_limit:
-            url = await remove_query_param(url, self.limit_query_param)
-        else:
-            url = await replace_query_param(url, self.limit_query_param, self.limit)
-        return await self.encode_url_parameters(url)
+            raise NotFound(msg)
 
-    async def get_previous_link(self):
-        if self.offset <= 0:
-            return None
-        elif self.offset - self.limit <= 0:
-            url = await remove_query_param(
-                await self.get_absolute_uri(), 
-                self.offset_query_param
-            )
-        else:
-            url = await replace_query_param(
-                await self.get_absolute_uri(), 
-                self.offset_query_param, 
-                self.offset - self.limit
-            )
-        if self.limit == self.default_limit:
-            url = await remove_query_param(url, self.limit_query_param)
-        else:
-            url = await replace_query_param(url, self.limit_query_param, self.limit)
-        return await self.encode_url_parameters(url)
-    
-    async def get_last_link(self):
-        url = await replace_query_param(
-            await self.get_absolute_uri(), 
-            self.offset_query_param, 
-            self.count // self.limit * self.limit
-        )
-        if self.limit == self.default_limit:
-            url = await remove_query_param(url, self.limit_query_param)
-        else:
-            url = await replace_query_param(url, self.limit_query_param, self.limit)
-        return await self.encode_url_parameters(url)
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
 
-    async def get_limit(self, request):
-        if self.limit_query_param:
-            with suppress(KeyError, ValueError):
-                return await self.positive_int(
-                    request.query_params[self.limit_query_param],
-                    strict=True,
-                    cutoff=self.max_limit
-                )
-        return self.default_limit
+        return self.page
 
-    async def get_offset(self, request):
+
+class LimitOffsetPagination(DRFLimitOffsetPagination):
+    async def aget_count(self, queryset):
         try:
-            return await self.positive_int(
-                request.query_params[self.offset_query_param],
-            )
-        except (KeyError, ValueError):
-            return 0
-    
-    async def get_count(self, queryset):
-        try:
-            return await queryset.acount()
+            return (await queryset.acount())
         except (AttributeError, TypeError):
             return len(queryset)
 
-if __name__ != '__main__':
-    LimitOffsetAsyncPagination = sync_to_async(LimitOffsetAsyncPagination).func()
+    async def paginate_queryset(self, queryset, request, view=None):
+        self.request = request
+        self.limit = self.get_limit(request)
+        if self.limit is None:
+            return None
+
+        self.count = await self.aget_count(queryset)
+        self.offset = self.get_offset(request)
+        if self.count > self.limit and self.template is not None:
+            self.display_page_controls = True
+
+        if self.count == 0 or self.offset > self.count:
+            return []
+        return queryset[self.offset:self.offset + self.limit]
+
+
+class CursorPagination(DRFCursorPagination):
+    async def paginate_queryset(self, queryset, request, view=None):
+        self.request = request
+        self.page_size = self.get_page_size(request)
+        if not self.page_size:
+            return None
+
+        self.base_url = request.build_absolute_uri()
+        self.ordering = self.get_ordering(request, queryset, view)
+
+        self.cursor = self.decode_cursor(request)
+        if self.cursor is None:
+            (offset, reverse, current_position) = (0, False, None)
+        else:
+            (offset, reverse, current_position) = self.cursor
+
+        if reverse:
+            queryset = queryset.order_by(*_reverse_ordering(self.ordering))
+        else:
+            queryset = queryset.order_by(*self.ordering)
+
+        if current_position is not None:
+            order = self.ordering[0]
+            is_reversed = order.startswith('-')
+            order_attr = order.lstrip('-')
+
+            if self.cursor.reverse != is_reversed:
+                kwargs = {order_attr + '__lt': current_position}
+            else:
+                kwargs = {order_attr + '__gt': current_position}
+
+            queryset = queryset.filter(**kwargs)
+
+        results = queryset[offset:offset + self.page_size + 1]
+        self.page = results[:self.page_size]
+
+        if (await results.acount()) > (await self.page.acount()):
+            has_following_position = True
+            num_elements = await results.acount()
+            async for item in queryset[offset + num_elements:offset + num_elements + 1]:
+                instance = item
+                break
+            following_position = self._get_position_from_instance(
+                instance,
+                self.ordering,
+            )
+        else:
+            has_following_position = False
+            following_position = None
+
+        if reverse:
+            self.has_next = (current_position is not None) or (offset > 0)
+            self.has_previous = has_following_position
+            if self.has_next:
+                self.next_position = current_position
+            if self.has_previous:
+                self.previous_position = following_position
+        else:
+            self.has_next = has_following_position
+            self.has_previous = (current_position is not None) or (offset > 0)
+            if self.has_next:
+                self.next_position = following_position
+            if self.has_previous:
+                self.previous_position = current_position
+
+        if (self.has_previous or self.has_next) and self.template is not None:
+            self.display_page_controls = True
+
+        return self.page
