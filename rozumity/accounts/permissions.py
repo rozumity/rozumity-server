47d2a685a43a927aa3988f26a30d62a2520b9568
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-14 13:22:42 +0000

    Refactor gender field

--- rozumity/accounts/permissions.py
+++ rozumity/accounts/permissions.py
@@ -4,6 +4,38 @@ from rozumity.permissions import AuthReadStaffWritePermission
 from accounts.models import TherapyContract
 
 
+class IsContractSignerPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.email in (obj.client_email, obj.expert_email)
+
+
+class IsProfileOwnerPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.email == obj.email
+
+
+class IsStaffPermission(permissions.BasePermission):
+    async def has_permission(self, request, view):
+        return request.user.is_staff
+
+
+class IsUserReadPermission(permissions.BasePermission):
+    async def has_permission(self, request, view):
+        return all((
+            request.user.is_authenticated, 
+            request.method == 'GET'
+        ))
+
+
+class IsStaffReadPermission(permissions.BasePermission):
+    async def has_permission(self, request, view):
+        return all((
+            request.user.is_authenticated,
+            request.user.is_staff,
+            request.method == 'GET'
+        ))
+
+
 class HasDiaryPermission(AuthReadStaffWritePermission):
     async def has_permission(self, request, view):
         has_diary_perm = False
@@ -58,8 +90,3 @@ class HasDyagnosisPermission(permissions.BasePermission):
                 has_dyagnosis_perm = True
                 break
         return has_dyagnosis_perm
-
-
-class IsContractSigner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user.email in (obj.client_email, obj.expert_email)
