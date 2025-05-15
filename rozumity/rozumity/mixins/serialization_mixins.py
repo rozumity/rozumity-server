2b560f44cc02f30f6d5a0776cc1ebf9a5d47bf78
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-15 21:25:02 +0000

    Add double serializator mixin

--- rozumity/rozumity/mixins/serialization_mixins.py
+++ rozumity/rozumity/mixins/serialization_mixins.py
@@ -5,3 +5,13 @@ from django_countries.serializers import CountryFieldMixin
 class CountryFieldMixinAsync(CountryFieldMixin):
     async def build_standard_field(self, field_name, model_field):
         return sync_to_async(super().build_standard_field)(field_name, model_field)
+
+
+class ReadWriteDiffMixin:
+    def get_serializer_class(self):
+        read, write = self.serializer_class[0], self.serializer_class[1]
+        if self.request.method == 'GET':
+            self.serializer_class = read
+            return read
+        self.serializer_class = write
+        return write
