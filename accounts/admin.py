90a993d60e035c0bbd5812faeb3ef6a7b776dbf0
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-18 21:54:08 +0000

    Add Custom User Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Permission
 
 from .models import (
     User, ClientProfile, ExpertProfile, StaffProfile, 
-    University, Speciality, TherapyContract, 
-    SubscriptionPlan, Education
+    TherapyContract, SubscriptionPlan
 )
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'date_joined', 'is_client', 'is_expert', 'is_staff', 'is_active')
+    list_filter = ('email', 'date_joined', 'is_client', 'is_expert', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth',)}),
+    )
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'is_client', 'is_expert', 'is_staff')}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('date_joined',)}),
+    )
+    
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+@admin.register(Permission)
+class CustomPermissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'content_type', 'codename')
+    search_fields = ('name', 'codename')
+    list_filter = ('content_type',)
 
 
 @admin.register(ClientProfile)
@@ -27,16 +51,6 @@ class StaffProfileAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(University)
-class UniversityAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Speciality)
-class SpecialityAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(TherapyContract)
 class TherapyContractAdmin(admin.ModelAdmin):
     pass
@@ -44,9 +58,4 @@ class TherapyContractAdmin(admin.ModelAdmin):
 
 @admin.register(SubscriptionPlan)
 class SubscriptionPlanAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Education)
-class EducationAdmin(admin.ModelAdmin):
     pass
