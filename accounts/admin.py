c08f18e2e106a8889559a60f2551bb293f1e5789
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-18 22:23:11 +0000

    Add jwt to user admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,10 +6,27 @@ from .models import (
     User, ClientProfile, ExpertProfile, StaffProfile, 
     TherapyContract, SubscriptionPlan
 )
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    def jwt_tokens(self, obj):
+        refresh = RefreshToken.for_user(obj)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+        return (  
+            f"Access: {str('\n'.join(
+                access_token[i:i+100] for i 
+                in range(0, len(access_token), 100
+            )))}\n\n"
+            f"Refresh: {str('\n'.join(
+                refresh_token[i:i+100] for i 
+                in range(0, len(refresh_token), 100
+            )))}"
+        )
+
+    jwt_tokens.short_description = "JWT Tokens"
+
     list_display = ('email', 'date_joined', 'is_client', 'is_expert', 'is_staff', 'is_active')
     list_filter = ('email', 'date_joined', 'is_client', 'is_expert', 'is_active')
     fieldsets = UserAdmin.fieldsets + (
@@ -20,13 +37,14 @@ class CustomUserAdmin(UserAdmin):
     )
     ordering = ('email',)
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'is_client', 'is_expert', 'is_staff')}),
+        (None, {'fields': ('email', 'password', 'jwt_tokens', 'is_client', 'is_expert', 'is_staff')}),
         ('Permissions', {'fields': ('is_active', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('date_joined',)}),
     )
     
     search_fields = ('email',)
     ordering = ('email',)
+    readonly_fields = ('jwt_tokens',)
 
 
 @admin.register(Permission)
