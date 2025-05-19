180030d1936a79327838aa5a97775ddb8f138500
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-19 16:53:31 +0000

    Refactor user creation manager

--- accounts/managers.py
+++ accounts/managers.py
@@ -9,6 +9,7 @@ class EmailUserManager(BaseUserManager):
         if not email:
             raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
+        extra_fields.setdefault("is_staff", False)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
@@ -17,6 +18,7 @@ class EmailUserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_staff", True)
 
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
