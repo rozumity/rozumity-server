b566900c0ad64d00ddc6cdc232ac2071b679bfac
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-27 01:10:39 +0000

    Create IsResponsePublic permission

--- screening/permissions.py
+++ screening/permissions.py
@@ -1,15 +1,25 @@
-
-from rest_framework import permissions
-
-from accounts.utils import get_profile
+from rest_framework.permissions import BasePermission
+from rozumity.utils import rel
 
 
-class IsResponseOwner(permissions.BasePermission):
+class IsResponsePublic(BasePermission):
     async def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            profile = await get_profile(request)
-            pk = view.kwargs.get('pk')
-            async for education in profile.education.all():
-                if education.id == pk:
-                    return True
+        if not request.user.is_authenticated:
             return False
+        if request.method == "POST":
+            return request.user.email == request.data.email
+        elif request.method in ["GET", "PUT", "PATCH"]:
+            email = request.user.email
+            model = view.serializer_class.Meta.model
+            obj = await model.objects.aget(**{
+                model._meta.pk.name: view.kwargs.get('pk')
+            })
+            client = await rel(obj, "client")
+            is_client_owner = await client.user_email == email
+            if obj.is_public and request.method == "GET":
+                return True
+            elif obj.is_public_expert and await client.expert_email == email:
+                return True
+            else:
+                return is_client_owner
+        return False
