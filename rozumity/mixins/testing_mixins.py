32d2e7dbcb5e3f495fae08d10fce405ecc970fb5
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-27 01:09:46 +0000

    Refactor testing mixins

--- rozumity/mixins/testing_mixins.py
+++ rozumity/mixins/testing_mixins.py
@@ -1,7 +1,10 @@
 # python manage.py test
 # python ../manage.py test accounts
+from httpx import AsyncClient, ASGITransport
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 from asgiref.sync import sync_to_async, async_to_sync
+from rozumity.asgi import application
 from accounts.models import ClientProfile, ExpertProfile
 from educations.models import *
 
@@ -12,6 +15,7 @@ class ProfileCreationMixin:
         return {
             "client": "client@user.com",
             "expert": "expert@user.com",
+            "staff": "staff@user.com"
         }
     
     @staticmethod
@@ -23,52 +27,37 @@ class ProfileCreationMixin:
         return await sync_to_async(get_user_model)()
 
     @classmethod
-    async def create_test_user_client(cls):
-        User = await cls.get_user_model()
-        return await sync_to_async(User.objects.create_user)(
-            email=cls.get_emails()["client"],
-            password=cls.get_password(),
+    def setUpClass(cls):
+        super().setUpClass()
+        user_model = get_user_model()
+        emails, password = cls.get_emails(), cls.get_password()
+        cls.user_client = user_model.objects.create_user(
+            email=emails["client"],
+            password=password,
             is_client=True
         )
-
-    @classmethod
-    async def create_test_user_expert(cls):
-        User = await cls.get_user_model()
-        return await sync_to_async(User.objects.create_user)(
-            email=cls.get_emails()["expert"],
-            password=cls.get_password(),
+        cls.user_expert = user_model.objects.create_user(
+            email=emails["expert"], password=password,
             is_expert=True
         )
+        cls.user_staff = user_model.objects.create_superuser(
+            email=emails["staff"], password=password
+        )
 
-    @classmethod
-    async def create_test_users(cls):
-        return await cls.create_test_user_client(), await cls.create_test_user_expert()
-
-    @classmethod
-    async def create_test_client(cls):
-        user = await cls.create_test_user_client()
-        return await ClientProfile.objects.aget(user=user)
-
-    @classmethod
-    async def create_test_expert(cls):
-        user = await cls.create_test_user_expert()
-        return await ExpertProfile.objects.aget(user=user)
-
-    @classmethod
-    async def create_test_profiles(cls):
-        user_client, user_expert = await cls.create_test_users()
-        client = await ClientProfile.objects.aget(user=user_client)
-        expert = await ExpertProfile.objects.aget(user=user_expert)
-        return client, expert
-
-    @classmethod
-    def create_test_user_client_sync(cls):
-        return async_to_sync(cls.create_test_user_client)()
-
-    @classmethod
-    def create_test_user_expert_sync(cls):
-        return async_to_sync(cls.create_test_user_expert)()
-
-    @classmethod
-    def create_test_profile_client_sync(cls):
-        return async_to_sync(cls.create_test_client)()
+    def setUp(self):
+        self.profile_client = ClientProfile.objects.get(
+            user=self.user_client
+        )
+        self.profile_expert = ExpertProfile.objects.get(
+            user=self.user_expert
+        )
+        self.api_client = AsyncClient(
+            transport=ASGITransport(app=application), 
+            base_url="http://testserver"
+        )
+        self.token_client = str(RefreshToken.for_user(
+            self.user_client
+        ).access_token)
+        self.token_expert = str(RefreshToken.for_user(
+            self.user_expert
+        ).access_token)
