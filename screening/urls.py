f28094dbe6decc1d8de99a9d59435519c4e2de8f
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-29 14:39:16 +0000

    Refactor question and response urls

--- screening/urls.py
+++ screening/urls.py
@@ -12,11 +12,11 @@ router.register(r"questionaries", views.QuestionaryListView, basename="questiona
 router.register(r"questionary", views.QuestionaryRetrieveView, basename="questionaries-questionary")
 router.register(r"dimensions", views.QuestionaryDimensionListView, basename="questionaries-dimensions")
 router.register(r"dimension", views.QuestionaryDimensionRetrieveView, basename="questionaries-dimension")
-router.register(r"questions", views.QuestionaryQuestionListCreateView, basename="questionaries-questions")
+router.register(r"questions", views.QuestionaryQuestionListView, basename="questionaries-questions")
 router.register(r"question", views.QuestionaryQuestionRetrieveView, basename="questionaries-question")
 router.register(r"answers", views.QuestionaryAnswerListCreateView, basename="questionaries-answers")
 router.register(r"answer", views.QuestionaryAnswerRetrieveView, basename="questionaries-answer")
-router.register(r"responses", views.QuestionaryResponseCreateView, basename="questionaries-responses")
+router.register(r"responses", views.QuestionaryResponseListCreateView, basename="questionaries-responses")
 router.register(r"response", views.QuestionaryResponseRetrieveUpdateView, basename="questionaries-response")
 
 urlpatterns = [
