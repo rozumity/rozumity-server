82012e974561ba50b99bb890ff692801e93c0b08
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-05-29 08:50:44 +0000

    Remove unused mixins

--- rozumity/mixins/serialization_mixins.py
+++ rozumity/mixins/serialization_mixins.py
@@ -7,16 +7,6 @@ class CountryFieldMixinAsync(CountryFieldMixin):
         return sync_to_async(super().build_standard_field)(field_name, model_field)
 
 
-class ReadWriteDiffMixin:
-    def get_serializer_class(self):
-        if hasattr(self, 'serializer_class_write'):
-            if self.request.method != 'GET':
-                self.serializer_class = self.serializer_class_write
-                delattr(self, 'serializer_class_write')
-            return self.serializer_class
-        return self.serializer_class
-
-
 class ReadOnlySerializerMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
