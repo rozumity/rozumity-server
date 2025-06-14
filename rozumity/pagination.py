f491ef4065d61cadc5103d19edc387e1d9eb2211
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-06-14 18:11:30 +0000

    Refactor all the async paginations

--- rozumity/pagination.py
+++ rozumity/pagination.py
@@ -1,7 +1,3 @@
-"""
-Pagination serializers determine the structure of the output that should
-be used for paginated responses.
-"""
 from django.core.paginator import InvalidPage
 from rest_framework.exceptions import NotFound
 from rest_framework.pagination import (
@@ -18,32 +14,37 @@ class PageNumberPagination(DRFPageNumberPagination):
         if not page_size:
             return None
 
-        paginator = self.django_paginator_class(queryset, page_size)
-        page_number = self.get_page_number(request, paginator)
-
-        try:
-            self.page = paginator.page(page_number)
-        except InvalidPage as exc:
-            msg = self.invalid_page_message.format(
-                page_number=page_number, message=str(exc)
-            )
-            raise NotFound(msg)
-
-        if paginator.num_pages > 1 and self.template is not None:
-            # The browsable API should display pagination controls.
-            self.display_page_controls = True
-
-        return self.page
+        if hasattr(queryset, "acount"):
+            count = await queryset.acount()
+            page_number = self.get_page_number(request, None)
+            start = (int(page_number) - 1) * page_size
+            end = start + page_size
+            page = [item async for item in queryset[start:end]]
+            self.page = page
+            self.display_page_controls = count > page_size and self.template is not None
+            return page
+        else:
+            paginator = self.django_paginator_class(queryset, page_size)
+            page_number = self.get_page_number(request, paginator)
+            try:
+                self.page = paginator.page(page_number)
+            except InvalidPage as exc:
+                msg = self.invalid_page_message.format(
+                    page_number=page_number, message=str(exc)
+                )
+                raise NotFound(msg)
+            self.display_page_controls = paginator.num_pages > 1 and self.template is not None
+            return self.page
 
 
 class LimitOffsetPagination(DRFLimitOffsetPagination):
     async def aget_count(self, queryset):
-        try:
-            return (await queryset.acount())
-        except (AttributeError, TypeError):
-            return len(queryset)
+        if hasattr(queryset, "acount"):
+            return await queryset.acount()
+        return len(await queryset)
 
     async def paginate_queryset(self, queryset, request, view=None):
+        queryset = await queryset
         self.request = request
         self.limit = self.get_limit(request)
         if self.limit is None:
@@ -56,7 +57,8 @@ class LimitOffsetPagination(DRFLimitOffsetPagination):
 
         if self.count == 0 or self.offset > self.count:
             return []
-        return queryset[self.offset:self.offset + self.limit]
+
+        return [item async for item in queryset[self.offset:self.offset + self.limit]]
 
 
 class CursorPagination(DRFCursorPagination):
@@ -71,9 +73,9 @@ class CursorPagination(DRFCursorPagination):
 
         self.cursor = self.decode_cursor(request)
         if self.cursor is None:
-            (offset, reverse, current_position) = (0, False, None)
+            offset, reverse, current_position = 0, False, None
         else:
-            (offset, reverse, current_position) = self.cursor
+            offset, reverse, current_position = self.cursor
 
         if reverse:
             queryset = queryset.order_by(*_reverse_ordering(self.ordering))
@@ -84,30 +86,21 @@ class CursorPagination(DRFCursorPagination):
             order = self.ordering[0]
             is_reversed = order.startswith('-')
             order_attr = order.lstrip('-')
-
             if self.cursor.reverse != is_reversed:
                 kwargs = {order_attr + '__lt': current_position}
             else:
                 kwargs = {order_attr + '__gt': current_position}
-
             queryset = queryset.filter(**kwargs)
 
-        results = queryset[offset:offset + self.page_size + 1]
+        results = [item async for item in queryset[offset:offset + self.page_size + 1]]
         self.page = results[:self.page_size]
 
-        if (await results.acount()) > (await self.page.acount()):
-            has_following_position = True
-            num_elements = await results.acount()
-            async for item in queryset[offset + num_elements:offset + num_elements + 1]:
-                instance = item
-                break
-            following_position = self._get_position_from_instance(
-                instance,
-                self.ordering,
-            )
-        else:
-            has_following_position = False
-            following_position = None
+        has_following_position = len(results) > self.page_size
+        following_position = None
+
+        if has_following_position:
+            instance = results[self.page_size]
+            following_position = self._get_position_from_instance(instance, self.ordering)
 
         if reverse:
             self.has_next = (current_position is not None) or (offset > 0)
