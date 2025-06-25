2a9109ea25fb1f9d959b250a9e5a5bf2ba982721
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-06-25 00:52:27 +0000

    Create signals for questionary completion

--- screening/signals.py
+++ screening/signals.py
@@ -1,96 +1,108 @@
+from hashlib import md5
 from collections import defaultdict
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-from django.db.models.signals import m2m_changed
-from asgiref.sync import sync_to_async
-
+from django.core.cache import cache
+from screening.models import QuestionaryResponse
 from rozumity.utils import rel
 
-from .models import QuestionaryResponse, QuestionaryScore, QuestionaryScoreExtra
+
+@receiver(post_save, sender=QuestionaryResponse, dispatch_uid='response.changed')
+async def handle_response_answers_change(sender, instance, created, **kwargs):
+    answers = getattr(instance, '_answers_from_request', [])
+    scores_map = getattr(instance, '_scores_map', [])
+    #await cache.adelete(
+    #    f"{md5(instance.__class__.__name__.lower().encode()).hexdigest()}:{instance.pk}"
+    #)
+    if isinstance(answers, list) and len(answers):
+        through = QuestionaryResponse.answers.through
+        await through.objects.abulk_create([
+            through(response=instance, answer=answer) for answer in answers
+        ], ignore_conflicts=True)
+        await m2m_changed.asend_robust(
+            sender=through, instance=instance, action='post_add', reverse=False, 
+            model=through, using='default', pk_set={answer.pk for answer in answers}
+        )
+    elif isinstance(scores_map, dict) and len(scores_map.keys()):
+        scores_map = instance.scores_map or {}
+        all_scores = [s async for s in instance.questionary.scores.all()]
+        scores_by_dim = defaultdict(list)
+        for score in all_scores:
+            scores_by_dim[str(score.dimension_id)].append(score)
+        found_scores = [min([
+            s for s in scores_by_dim.get(str(dim_id), [])
+            if s.min_score < value <= s.max_score
+        ], key=lambda s: (
+            s.max_score - s.min_score,
+            abs((s.max_score + s.min_score) / 2 - value)
+        )) for dim_id, value in scores_map.items()]
+        if found_scores:
+            await instance.scores.aadd(found_scores[0].pk)
+    else:
+        await cache.adelete(
+            f"{md5(instance.__class__.__name__.lower().encode()).hexdigest()}:{instance.pk}"
+        )
 
 
-@receiver(m2m_changed, sender=QuestionaryResponse.answers.through, dispatch_uid='response.answer_changed')
-async def response_answer_changed(sender, instance, action, **kwargs):
-    # Check that all answers are provided (is_filled)
-    if not await instance.is_filled:
+@receiver(m2m_changed, sender=QuestionaryResponse.answers.through, dispatch_uid='response.changed_answers')
+async def calc_response_scores_map(sender, instance, action, reverse, model, pk_set, **kwargs):
+    if action != 'post_add':
         return
-
-    # 1. Collect dimension values from the user's answers
-    dimension_values = {dim_id: value for dim_id, value in await sync_to_async(list)(
-        instance.answers.through.objects.filter(
-            answer_id__in=await sync_to_async(list)(
-                instance.answers.values_list('id', flat=True)
-            )
-        ).values_list(
-            'questionarydimensionvalue__dimension_id', 
-            'questionarydimensionvalue__value'
-        )
-    )}
-
-    # 2. Fetch all matching scores in a single query
-    all_scores = await sync_to_async(list)(
-        QuestionaryScore.objects.filter(
-            questionary=instance.questionary,
-            dimension_id__in=dimension_values.keys()
-        )
+    try:
+        del instance._answers_from_request
+    except AttributeError:
+        pass
+    if instance.is_filled:
+        instance.is_filled, instance.is_checked = False, False
+        await instance.asave()
+        await instance.scores.aclear()
+        await instance.scores_extra.aclear()
+    questionary = await rel(instance, 'questionary')
+    questions_q = questionary.questions.all()
+    answers_q = instance.answers.all()
+    if await answers_q.acount() >= await questions_q.acount():
+        question_ids = [q async for q in questions_q.values_list("id", flat=True)]
+        answer_question_ids = [a async for a in answers_q.values_list("question_id", flat=True)]
+        is_filled = set(question_ids).issubset(set(answer_question_ids))
+        if is_filled:
+            instance.is_filled = is_filled
+            await instance.asave()
+    if not instance.is_filled:
+        return
+    answer_ids = [id async for id in instance.answers.values_list('id', flat=True)]
+    dimvals_model = instance._meta.get_field('answers').remote_field.model._meta.get_field('dimensions').remote_field.through
+    dimvals = [
+        d async for d in dimvals_model.objects.filter(answer_id__in=answer_ids)
+        .values('dimension_id', 'value')
+    ]
+    scores_map = defaultdict(float)
+    for d in dimvals:
+        scores_map[d['dimension_id']] += d['value']
+    instance._scores_map = {dim_id: value for dim_id, value in scores_map.items()}
+    instance.scores_map = instance._scores_map
+    await cache.adelete(
+        f"{md5(instance.__class__.__name__.lower().encode()).hexdigest()}:{instance.pk}"
     )
+    await instance.answers.aclear()
+    await instance.asave()
 
-    # 2.1. Group scores by dimension_id
-    scores_by_dim = defaultdict(list)
-    for score in all_scores:
-        scores_by_dim[score.dimension_id].append(score)
 
-    # 2.2. For each dimension, select the best score (narrowest range, closest to the value)
-    found_scores = []
-    for dim_id, value in dimension_values.items():
-        candidates = [
-            s for s in scores_by_dim.get(dim_id, [])
-            if s.min_score <= value <= s.max_score
-        ]
-        if candidates:
-            found_scores.append(min(
-                candidates,
-                key=lambda s: (
-                    s.max_score - s.min_score,
-                    abs((s.max_score + s.min_score) / 2 - value)
-                )
-            ))
-    await instance.scores.aset(found_scores)
-
-    # 3. Check extra results (whether all dimensions match)
-    async for extra in QuestionaryScoreExtra.objects.filter(questionary=instance.questionary).prefetch_related('scores').all():
-        extra_dim_ids = set(es.dimension_id for es in list(extra.scores.all()))
-        if extra_dim_ids and extra_dim_ids.issubset(set(s.dimension_id for s in found_scores)):
-            await instance.scores_extra.aset([extra])
-            break  # Only the first matching one
-    
-
-@receiver(m2m_changed, sender=QuestionaryResponse.scores.through, dispatch_uid='response.scores_changed')
-async def scores_changed(sender, instance, action, **kwargs):
-    if action not in ['post_add', 'post_remove', 'post_clear']:
+@receiver(m2m_changed, sender=QuestionaryResponse.scores.through, dispatch_uid='response.changed_scores')
+async def handle_response_scores_changed(sender, instance, action, reverse, model, pk_set, **kwargs):
+    if action != 'post_add':
         return
-
-    # 1. Get all dimension IDs that already have a score
-    scored_dim_ids = set(await sync_to_async(list)(
-        instance.scores.all().values_list('dimension_id', flat=True)
-    ))
-
-    # 2. Assign first matching scores_extra (if its required dimensions are all scored)
-    async for extra in QuestionaryScoreExtra.objects.filter(
+    try:
+        del instance._scores_map
+    except AttributeError:
+        pass
+    score_ids = set([s.id async for s in instance.scores.all()])
+    extra_scores_model = instance._meta.get_field('scores_extra').remote_field.model
+    extras = [e async for e in extra_scores_model.objects.filter(
         questionary=instance.questionary
-    ).prefetch_related('scores'):
-        extra_dim_ids = {
-            s.dimension_id for s 
-            in await sync_to_async(list)(extra.scores.all())
-        }
-        if extra_dim_ids and extra_dim_ids.issubset(scored_dim_ids):
+    ).prefetch_related('scores')]
+    for extra in extras:
+        extra_score_ids = set([s.id for s in extra.scores.all()])
+        if extra_score_ids and extra_score_ids.issubset(score_ids):
             await instance.scores_extra.aset([extra])
-            break
-
-    # 3. Check completeness: all dimensions from answers must be scored
-    if set(await sync_to_async(list)(instance.answers.through.objects.filter(
-        questionaryresponse_id=instance.pk
-    ).values_list(
-        'questionarydimensionvalue__dimension_id', flat=True
-    ).distinct())).issubset(scored_dim_ids):
-        instance.is_checked = True
-        await instance.asave(update_fields=["is_checked"])
+    instance.is_checked = True
+    await instance.asave()
