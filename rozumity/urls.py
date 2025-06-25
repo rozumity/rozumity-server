b4eaea595e0543b1b3f2fc23bc3be4b171f5060c
Mariia Hladka <mariia.hladka@agroapp.com.ua>
2025-06-25 00:46:09 +0000

    Add ckeditor urls

--- rozumity/urls.py
+++ rozumity/urls.py
@@ -12,6 +12,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls), # admin site
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
