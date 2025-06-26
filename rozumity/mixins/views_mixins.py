b6d16f56778d0e6f0f6d903f812640ffdde7e80e
imgVOID <imgvoid@gmail.com>
2025-06-26 11:11:32 +0000

    Refactor views mixins

--- rozumity/mixins/views_mixins.py
+++ rozumity/mixins/views_mixins.py
@@ -1,5 +1,7 @@
+from asgiref.sync import sync_to_async
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from adrf.viewsets import GenericViewSet
+from adrf.generics import aget_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rozumity.mixins.caching_mixins import (
     ListModelMixin, CreateModelMixin, RetrieveModelMixin,
@@ -52,6 +54,23 @@ class AsyncModelViewSet(
             self.serializer_class.ais_valid = AsyncSerializerMixin.ais_valid
         return self
 
+    async def aget_object(self):
+        queryset = self.filter_queryset(await self.get_queryset())
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        assert lookup_url_kwarg in self.kwargs, (
+            "Expected view %s to be called with a URL keyword argument "
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            "attribute on the view correctly."
+            % (self.__class__.__name__, lookup_url_kwarg)
+        )
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = await aget_object_or_404(queryset, **filter_kwargs)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    async def get_queryset(self):
+        return await sync_to_async(GenericViewSet.get_queryset)(self)
+
 
 class AsyncReadOnlyModelViewSet(
     ListModelMixin,
@@ -62,3 +81,12 @@ class AsyncReadOnlyModelViewSet(
 
     def __new__(cls, *args, **kwargs):
         return AsyncModelViewSet.__new__(cls, *args, **kwargs)
+
+    async def aget_object(self):
+        return await AsyncModelViewSet.aget_object(self)
+
+    async def aget_object(self):
+        return await AsyncModelViewSet.aget_object(self)
+
+    async def get_queryset(self):
+        return await AsyncModelViewSet.get_queryset(self)
