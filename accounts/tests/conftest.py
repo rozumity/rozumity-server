c1f8e648bb067a95e703b525cfc1aa2bbd75f72e
imgvoid <imgvoid@gmail.com>
2026-02-28 08:35:19 +0200

    Move conftest

--- accounts/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest_asyncio
-from adrf.test import AsyncAPIClient
-
-@pytest_asyncio.fixture
-async def adrf_client():
-    """
-    Fixture for the asynchronous DRF APIClient from 'adrf' package.
-    """
-    return AsyncAPIClient()
